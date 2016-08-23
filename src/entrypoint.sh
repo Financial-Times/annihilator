a36f577efee9149b38aea04318b7b81b65693b43
jspc <james@zero-internet.org.uk>
2016-08-23 14:15:53 +0100

    Output config prior to application of config

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -12,4 +12,6 @@ sleep ${SLEEP}   # Oftentimes we want to defer this until stuff is running
 
 /consul-template -consul=${CONSUL} -template=template/haproxy.cfg.tmpl:/annihilator/haproxy.cfg -once
 
+cat /annihilator/haproxy.cfg
+
 exec haproxy -V -f /annihilator/haproxy.cfg
