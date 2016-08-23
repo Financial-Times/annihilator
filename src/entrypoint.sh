e73df073abe49d819dd4fb6824f7ea37b87c40e2
jspc <james@zero-internet.org.uk>
2016-08-23 12:53:42 +0100

    Add optional sleep to startup

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,6 +6,9 @@ set -e
 
 
 : ${CONSUL:?}
+: ${SLEEP:0}
+
+sleep ${SLEEP}   # Oftentimes we want to defer this until stuff is running
 
 /consul-template -consul=${CONSUL} -template=template/haproxy.cfg.tmpl:/annihilator/haproxy.cfg -once
 
